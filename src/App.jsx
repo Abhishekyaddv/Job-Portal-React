34b89853eb4f72b3110e129b47b63f4931521e75
Abhishek Yadav <165659574+Abhishekyaddv@users.noreply.github.com>
2025-12-01 00:03:27 +0530

    Added Edit Option for each Job

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import JobsPage from './Pages/JobsPage';
 import NotfoundPage from './Pages/NotfoundPage';
 import JobPage, { jobLoader } from './Pages/JobPage';
 import AddJobPage from './Pages/AddJobPage'
-
+import EditJobPage from './Pages/EditJobPage';
 
 
 const App = () => {
@@ -30,17 +30,32 @@ const App = () => {
 // DELETE JOB
 
 const deleteJob = async(id) =>{
-  console.log('delet', id)
+  const res = await fetch(`/api/jobs/${id}`, {
+    method: 'DELETE',
+  });
+  return;
 }
 
-
+// Update Job
+const updateJob = async (job) =>{
+  const res = await fetch(`/api/jobs/${job.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(job)
+  });
+  return;
+}
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
   <Route index element={<HomePage />} />
   <Route path='/jobs' element={<JobsPage />} />
   <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
+  <Route path='/edit-job/:id' element={<EditJobPage updateJobSubmit = {updateJob} />} loader = {jobLoader} />
   <Route path='/jobs/:id' element={<JobPage deleteJob={ deleteJob } />} loader = {jobLoader} />
+
   <Route path='*' element={<NotfoundPage />} />
 
 
--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { ImOpt } from 'react-icons/im';
+
+import { toast } from 'react-toastify';
 // import { useState,useEffect } from 'react'
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
@@ -15,6 +15,7 @@ const JobPage = ({ deleteJob }) => {
 
     deleteJob(jobId)
 
+    toast.success('Job Deleted Sucessfully')
     navigate('/jobs')
   }
   // const [job, setJob] = useState(null);
@@ -110,7 +111,7 @@ const JobPage = ({ deleteJob }) => {
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
               <Link
-                to={`/jobs/edit/${job.id}`}
+                to={`/edit-job/${job.id}`}
                 className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link>
               <button onClick={()=> onDeleteClick(job.id)}
